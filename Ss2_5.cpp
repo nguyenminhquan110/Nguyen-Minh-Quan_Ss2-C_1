4a31755d33dbdf07b02cebfdbe0b4db02b21c66a
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-20 22:59:48 +0700

    Add files via upload

---
+++ Ss2_5.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+	float a = 1;
+	float b = 10;
+	float perimeter = (a+b)/2;
+	float area = a*b;
+	printf("Chu vi c?a hình ch? nh?t là: %.2f/n",perimeter);
+	printf("Di?n tích hình ch? nh?t là: %.2f?n",area);
+	return 0; 
+} 
