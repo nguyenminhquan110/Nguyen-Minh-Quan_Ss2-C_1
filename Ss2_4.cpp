562071d954a0c738797d5ee8722937407c89f2e1
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-20 22:38:08 +0700

    Add files via upload

---
+++ Ss2_4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float side = 1.0;
+    float perimeter = 4 * side;
+    float area = side * side; 
+    printf("C?nh c?a h?nh vuông: %.2f\n", side);
+    printf("Chu vi c?a h?nh vuông là: %.2f\n", perimeter);
+    printf("Di?n tích c?a h?nh vuông là: %.2f\n", area);
+    return 0;
+}
+
