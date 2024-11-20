e6b82547ed347bb14474d6dcc251c2675ee54272
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-20 22:28:11 +0700

    Add files via upload

---
+++ Ss2_2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 01;  
+    int num2 = 10; 
+    int sum = num1 + num2;
+    printf("T?ng c?a %d và %d là %d\n", num1, num2, sum);
+    
+    return 0;
+}
+
